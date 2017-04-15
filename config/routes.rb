dcf666f88cb31052af06a92f0f9da4daf2a77366
taduyvinh <taduyvinh@github.com>
2017-04-15 09:51:17 +0700

    chap 7

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
   root "static_pages#home"
+  resources :users, only: [:show]
   get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
 end
