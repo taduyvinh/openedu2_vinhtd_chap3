b386ca362573e1c627d14c310b4ab1966c782174
taduyvinh <taduyvinh@github.com>
2017-04-17 10:31:23 +0700

    chap 8

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
